dfa3c4048e7c8fe2e8264439a0e5b977f3a51753
MuhamadPrajitno <muhamadismuaji@gmail.com>
2018-01-16 11:27:00 +0700

    initial commit with login & logout done

---
+++ app/Http/Controllers/AuthAdmin/LoginController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\AuthAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('authAdmin.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::guard('admin')->attempt($credential, $request->member)) {
+            return redirect()->intended(route('admin.home'));
+        }
+
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+
+        return redirect('/');
+    }
+}
